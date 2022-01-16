d3df1df217ffab2c8bf701de814247a5d206f816
oitsjustjose <stovallj1995@gmail.com>
2022-01-16 12:11:35 -0600

    Fix itemstack size

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -5,3 +5,9 @@ onEvent("item.registry", (event) => {
     .create("cloth_covered_windmill_blade")
     .texture("kubejs:item/cloth_covered_windmill_blade");
 });
+
+onEvent("item.modification", (event) => {
+  event.modify("corail_woodcutter:acacia_woodcutter", (item) => {
+    item.maxStackSize = 1;
+  });
+});
