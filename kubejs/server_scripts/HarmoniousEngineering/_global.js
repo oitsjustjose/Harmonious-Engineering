8e9c12c98ebe008db4835df1fa1257a301c26a7b
oitsjustjose <stovallj1995@gmail.com>
2022-01-19 15:58:13 -0600

    Improved NatProg compat

--- kubejs/server_scripts/HarmoniousEngineering/_global.js
+++ kubejs/server_scripts/HarmoniousEngineering/_global.js
@@ -1,22 +1,57 @@
 // priority: 1024
 
 onEvent("server.datapack.first", (_) => {
-  global.minecraftColors = [
-    "white",
-    "orange",
-    "magenta",
-    "light_blue",
-    "yellow",
-    "lime",
-    "pink",
-    "gray",
-    "light_gray",
-    "cyan",
-    "purple",
-    "blue",
-    "brown",
-    "green",
-    "red",
-    "black",
-  ];
+  global.minecraftColors = ["white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown", "green", "red", "black"];
+
+  /* Creates */
+  global.genNatProgRecipes = (base, stripped, planks) => {
+    const stripLogWithSaw = {
+      type: "natural-progression:damage_tools",
+      ingredients: [
+        {
+          tag: "natural-progression:saw",
+        },
+        {
+          item: base,
+        },
+      ],
+      result: {
+        item: stripped,
+      },
+    };
+
+    const planksFromStrippedAxe = {
+      type: "natural-progression:damage_tools",
+      ingredients: [
+        {
+          item: stripped,
+        },
+        {
+          tag: "natural-progression:axe",
+        },
+      ],
+      result: {
+        item: planks,
+        count: 1,
+      },
+    };
+
+    const planksFromStrippedSaw = {
+      type: "natural-progression:damage_tools",
+      ingredients: [
+        {
+          item: stripped,
+        },
+        {
+          tag: "natural-progression:saw",
+        },
+      ],
+      result: {
+        item: planks,
+        count: 4,
+      },
+    };
+
+    return [stripLogWithSaw, planksFromStrippedAxe, planksFromStrippedSaw];
+  };
 });
--- kubejs/server_scripts/HarmoniousEngineering/recipes/tconstruct.js
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/tconstruct.js
@@ -95,4 +95,34 @@ onEvent("recipes", (event) => {
       count: 1,
     },
   });
+
+  const tconWoods = [
+    {
+      log: "tconstruct:greenheart_log",
+      strippedLog: "tconstruct:stripped_greenheart_log",
+      wood: "tconstruct:greenheart_wood",
+      strippedWood: "tconstruct:stripped_greenheart_wood",
+      plank: "tconstruct:greenheart_planks",
+    },
+    {
+      log: "tconstruct:skyroot_log",
+      strippedLog: "tconstruct:stripped_skyroot_log",
+      wood: "tconstruct:skyroot_wood",
+      strippedWood: "tconstruct:stripped_skyroot_wood",
+      plank: "tconstruct:skyroot_planks",
+    },
+    {
+      log: "tconstruct:bloodshroom_log",
+      strippedLog: "tconstruct:stripped_bloodshroom_log",
+      wood: "tconstruct:bloodshroom_wood",
+      strippedWood: "tconstruct:stripped_bloodshroom_wood",
+      plank: "tconstruct:bloodshroom_planks",
+    },
+  ];
+
+  tconWoods.forEach((woodType) => {
+    event.remove({ output: woodType.plank });
+    global.genNatProgRecipes(woodType.log, woodType.strippedLog, woodType.plank).forEach((x) => event.custom(x));
+    global.genNatProgRecipes(woodType.wood, woodType.strippedWood, woodType.plank).forEach((x) => event.custom(x));
+  });
 });
