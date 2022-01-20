969e923e164ec552eb60e78aebd67b33b62be0fb
oitsjustjose <stovallj1995@gmail.com>
2022-01-19 19:40:55 -0600

    Torchmaster, fix loading issue

---
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/torchmaster.js
@@ -0,0 +1,15 @@
+onEvent("recipes", (event) => {
+  event.remove({ output: "torchmaster:feral_flare_lantern" });
+  event.shaped("1x torchmaster:feral_flare_lantern", ["PPP", "GFG", "PPP"], {
+    P: "#forge:plates/lumium",
+    G: "#forge:glass",
+    F: "immersiveengineering:floodlight",
+  });
+
+  event.remove({ output: "torchmaster:megatorch" });
+  event.shaped("1x torchmaster:megatorch", ["FFF", "HLH", "HLH"], {
+    F: "torchmaster:feral_flare_lantern",
+    H: "#harmeng:meka/filler_1",
+    L: "#minecraft:logs",
+  });
+});
