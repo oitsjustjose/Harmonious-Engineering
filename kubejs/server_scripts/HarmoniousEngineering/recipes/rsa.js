737cb591c30d06d71cd70121fab3232c13ee921c
oitsjustjose <stovallj1995@gmail.com>
2022-01-25 10:37:15 -0600

    Add chapter for Redstone Arsenal

--- kubejs/server_scripts/HarmoniousEngineering/recipes/rsa.js
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/rsa.js
@@ -1,6 +1,7 @@
 onEvent("recipes", (event) => {
   event.remove({ id: "redstone_arsenal:materials/flux_gem" });
   event.remove({ id: "redstone_arsenal:materials/flux_dust" });
+  event.remove({ id: "redstone_arsenal:materials/flux_gear" });
   event.remove({ id: "redstone_arsenal:materials/flux_ingot_fire_charge_from_dust" });
   event.remove({ id: "redstone_arsenal:materials/obsidian_rod" });
   event.remove({ id: "redstone_arsenal:flux_elytra" });
@@ -24,6 +25,7 @@ onEvent("recipes", (event) => {
     })
   );
 
+  // Obsidian Rod using Rod Mold
   event.custom({
     type: "immersiveengineering:metal_press",
     mold: { item: "immersiveengineering:mold_rod" },
@@ -36,5 +38,34 @@ onEvent("recipes", (event) => {
     type: "thermal:press",
     ingredients: [{ tag: "forge:obsidian" }, { item: "immersiveengineering:mold_rod" }],
     result: [{ item: "redstone_arsenal:obsidian_rod" }],
+  });
+
+  // Manual Gear Crafting
+  event.custom({
+    type: "natural-progression:damage_tools",
+    ingredients: [
+      { item: "create:cogwheel" },
+      { item: "immersiveengineering:hammer" },
+      { item: "redstone_arsenal:flux_plating" },
+      { item: "redstone_arsenal:flux_plating" },
+      { item: "redstone_arsenal:flux_plating" },
+      { item: "redstone_arsenal:flux_plating" },
+    ],
+    result: { item: "redstone_arsenal:flux_gear" },
+  });
+
+  event.custom({
+    type: "thermal:press",
+    ingredients: [{ item: "redstone_arsenal:flux_ingot", count: 4 }, { item: "thermal:press_gear_die" }],
+    result: [{ item: "redstone_arsenal:flux_gear" }],
+  });
+
+  // IE Metal Press
+  event.custom({
+    type: "immersiveengineering:metal_press",
+    mold: { item: "immersiveengineering:mold_gear" },
+    input: { base_ingredient: { item: "redstone_arsenal:flux_ingot" }, count: 4 },
+    result: { item: "redstone_arsenal:flux_gear" },
+    energy: 4800,
   });
 });
---
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/tools_complement.js
@@ -0,0 +1,11 @@
+onEvent("recipes", (event) => {
+  event.remove({ id: "tools_complement:netherite_excavator" });
+  event.remove({ id: "tools_complement:netherite_hammer" });
+  event.remove({ id: "tools_complement:netherite_sickle" });
+  event.remove({ id: "tools_complement:netherite_knife" });
+
+  event.smithing("tools_complement:netherite_excavator", "tools_complement:diamond_excavator", "minecraft:netherite_ingot");
+  event.smithing("tools_complement:netherite_hammer", "tools_complement:diamond_hammer", "minecraft:netherite_ingot");
+  event.smithing("tools_complement:netherite_sickle", "tools_complement:diamond_sickle", "minecraft:netherite_ingot");
+  event.smithing("tools_complement:netherite_knife", "tools_complement:diamond_knife", "minecraft:netherite_ingot");
+});
