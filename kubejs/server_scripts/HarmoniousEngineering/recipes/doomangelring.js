1bd31992bea51010d1eaff7b2e857a8aaa2b47ed
oitsjustjose <stovallj1995@gmail.com>
2022-02-02 07:52:26 -0600

    Fix up DoomAngelRing

--- kubejs/server_scripts/HarmoniousEngineering/recipes/doomangelring.js
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/doomangelring.js
@@ -5,33 +5,15 @@ onEvent('recipes', event => {
     type: 'create:mechanical_crafting',
     pattern: ['FFBBBFF', 'FBNRNBF', 'BHSGSHB', 'BLT TLB', 'BHSGSHB', ' BNRNB ', '  BBB  '],
     key: {
-      B: {
-        item: 'create:brass_ingot',
-      },
-      S: {
-        item: 'minecraft:nether_star',
-      },
-      G: {
-        item: 'tconstruct:hepatizon_ingot',
-      },
-      R: {
-        item: 'create:refined_radiance',
-      },
-      L: {
-        item: 'alexsmobs:void_worm_eye',
-      },
-      H: {
-        item: 'buzzier_bees:bee_bottle',
-      },
-      N: {
-        item: 'minecraft:netherite_block',
-      },
-      T: {
-        item: 'alexsmobs:tarantula_hawk_wing',
-      },
-      F: {
-        item: 'minecraft:feather',
-      },
+      B: {tag: 'forge:ingots/brass'},
+      S: {item: 'minecraft:nether_star'},
+      G: {tag: 'forge:ingots/draconium'},
+      R: {item: 'create:refined_radiance'},
+      L: {item: 'alexsmobs:void_worm_eye'},
+      H: {item: 'buzzier_bees:bee_bottle'},
+      N: {item: 'minecraft:netherite_block'},
+      T: {item: 'alexsmobs:tarantula_hawk_wing'},
+      F: {item: 'minecraft:feather'},
     },
     result: {
       item: 'doomangelring:itemdoomangelring',
