ca23ce547c7d9b3cc930a30c95e4c1604b79f5f7
oitsjustjose <stovallj1995@gmail.com>
2022-02-02 07:43:23 -0600

    Clean up internal tags

--- kubejs/server_scripts/HarmoniousEngineering/recipes/torchmaster.js
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/torchmaster.js
@@ -9,7 +9,7 @@ onEvent('recipes', event => {
   event.remove({output: 'torchmaster:megatorch'});
   event.shaped('1x torchmaster:megatorch', ['FFF', 'HLH', 'HLH'], {
     F: 'torchmaster:feral_flare_lantern',
-    H: '#harmeng:meka/filler_1',
+    H: '#harmeng:meka/mid_tier_mat',
     L: '#minecraft:logs',
   });
 });
