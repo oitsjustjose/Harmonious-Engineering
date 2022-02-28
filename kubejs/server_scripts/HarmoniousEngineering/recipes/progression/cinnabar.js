0f9b5e967286913e0b1ad1d0d94e8c0c47f7c064
oitsjustjose <stovallj1995@gmail.com>
2022-02-28 08:17:02 -0600

    Rebalance cinnabar because everyone has too much redstone

--- kubejs/server_scripts/HarmoniousEngineering/recipes/progression/cinnabar.js
+++ kubejs/server_scripts/HarmoniousEngineering/recipes/progression/cinnabar.js
@@ -1,227 +1,91 @@
-/* Handles turning cinnabar into redstone in the many ways */
-const ieCrusher = event => {
+onEvent('recipes', event => {
+  const COUNTS = {
+    mill: 1,
+    crusher: 2,
+    machine: 3,
+    purify: 4,
+  };
+
+  /********************************
+   *          IE Crusher          *
+   *******************************/
   event.custom({
     type: 'immersiveengineering:crusher',
-    secondaries: [
-      {
-        chance: 0.6,
-        output: {
-          item: 'minecraft:redstone',
-        },
-      },
-    ],
-    result: {
-      count: 4,
-      base_ingredient: {
-        item: 'minecraft:redstone',
-      },
-    },
-    input: {
-      tag: 'forge:gems/cinnabar',
-    },
+    input: {tag: 'forge:ores/cinnabar'},
+    result: {count: COUNTS.machine, base_ingredient: {item: 'minecraft:redstone'}},
+    secondaries: [],
+    energy: 12000,
+  });
+  event.custom({
+    type: 'immersiveengineering:crusher',
+    input: {tag: 'forge:gems/cinnabar'},
+    result: {count: COUNTS.machine, base_ingredient: {item: 'minecraft:redstone'}},
+    secondaries: [],
     energy: 9000,
   });
 
-  event.custom({
-    type: 'immersiveengineering:crusher',
-    secondaries: [
-      {
-        chance: 0.6,
-        output: {
-          item: 'minecraft:redstone',
-        },
-      },
-    ],
-    result: {
-      count: 6,
-      base_ingredient: {
-        item: 'minecraft:redstone',
-      },
-    },
-    input: {
-      tag: 'forge:ores/cinnabar',
-    },
-    energy: 12000,
-  });
-};
-
-const mill = event => {
+  /**********************************
+   *             Create             *
+   **********************************/
+  // Milling Gem (No Ore Permitted)
   event.custom({
     type: 'create:milling',
-    ingredients: [
-      {
-        tag: 'forge:gems/cinnabar',
-      },
-    ],
-    results: [
-      {
-        item: 'minecraft:redstone',
-        count: 1,
-      },
-      {
-        item: 'minecraft:redstone',
-        count: 1,
-        chance: 0.5,
-      },
-    ],
-    processingTime: 500,
-    conditions: [
-      {
-        value: {
-          tag: 'forge:gems/cinnabar',
-          type: 'forge:tag_empty',
-        },
-        type: 'forge:not',
-      },
-    ],
+    ingredients: [{tag: 'forge:gems/cinnabar'}],
+    results: [{item: 'minecraft:redstone', count: COUNTS.mill}],
+    processingTime: 750,
   });
-};
-
-const crushingWheel = event => {
   event.custom({
     type: 'create:crushing',
-    ingredients: [
-      {
-        tag: 'forge:ores/cinnabar',
-      },
-    ],
+    ingredients: [{tag: 'forge:ores/cinnabar'}],
     results: [
-      {
-        item: 'minecraft:redstone',
-        count: 8,
-      },
-      {
-        item: 'minecraft:cobblestone',
-        chance: 0.3,
-      },
+      {item: 'minecraft:redstone', count: COUNTS.crusher},
+      {item: 'minecraft:cobblestone', chance: 0.3},
     ],
-    processingTime: 850,
-    conditions: [
-      {
-        value: {
-          tag: 'forge:ores/cinnabar',
-          type: 'forge:tag_empty',
-        },
-        type: 'forge:not',
-      },
-    ],
+    processingTime: 950,
   });
-
   event.custom({
     type: 'create:crushing',
-    ingredients: [
-      {
-        tag: 'forge:gems/cinnabar',
-      },
-    ],
-    results: [
-      {
-        item: 'minecraft:redstone',
-        count: 5,
-      },
-      {
-        item: 'minecraft:redstone',
-        count: 1,
-        chance: 0.3,
-      },
-    ],
+    ingredients: [{tag: 'forge:gems/cinnabar'}],
+    results: [{item: 'minecraft:redstone', count: COUNTS.crusher}],
     processingTime: 600,
-    conditions: [
-      {
-        value: {
-          tag: 'forge:gems/cinnabar',
-          type: 'forge:tag_empty',
-        },
-        type: 'forge:not',
-      },
-    ],
   });
-};
 
-const enrichmentChamber = event => {
+  /**********************************
+   *            Mekanism            *
+   **********************************/
   event.custom({
     type: 'mekanism:enriching',
-    input: {
-      ingredient: {
-        tag: 'forge:gems/cinnabar',
-      },
-    },
-    output: {
-      item: 'minecraft:redstone',
-      count: 7,
-    },
+    input: {ingredient: {tag: 'forge:gems/cinnabar'}},
+    output: {item: 'minecraft:redstone', count: COUNTS.machine},
   });
-};
-
-const purifier = event => {
   event.custom({
     type: 'mekanism:purifying',
-    itemInput: {
-      ingredient: {
-        tag: 'forge:gems/cinnabar',
-      },
-    },
-    gasInput: {
-      amount: 1,
-      gas: 'mekanism:oxygen',
-    },
-    output: {
-      item: 'minecraft:redstone',
-      count: 11,
-    },
+    itemInput: {ingredient: {tag: 'forge:gems/cinnabar'}},
+    gasInput: {amount: 1, gas: 'mekanism:oxygen'},
+    output: {item: 'minecraft:redstone', count: COUNTS.purify},
   });
-};
 
-const pulverizer = event => {
+  /***********************************
+   *             Thermal             *
+   **********************************/
+  // Remove the cinnabar dust recipe.
   event.remove({id: 'thermal:machine/pulverizer/pulverizer_cinnabar'});
   event.custom({
     type: 'thermal:pulverizer',
-    ingredient: {
-      tag: 'forge:ores/cinnabar',
-    },
+    ingredient: {tag: 'forge:ores/cinnabar'},
     result: [
-      {
-        item: 'minecraft:redstone',
-        count: 8,
-      },
-      {
-        item: 'minecraft:redstone',
-        count: 3,
-        chance: 0.85,
-      },
-      {
-        item: 'minecraft:gravel',
-        chance: 0.2,
-      },
+      {item: 'minecraft:redstone', count: COUNTS.machine},
+      {item: 'minecraft:gravel', chance: 0.2},
     ],
     experience: 1.4,
   });
-
   event.custom({
     type: 'thermal:pulverizer',
-    ingredient: {
-      tag: 'forge:gems/cinnabar',
-    },
+    ingredient: {tag: 'forge:gems/cinnabar'},
     result: [
-      {
-        item: 'minecraft:redstone',
-        count: 6,
-      },
-      {
-        item: 'minecraft:redstone',
-        count: 2,
-        chance: 0.65,
-      },
+      {item: 'minecraft:redstone', count: COUNTS.machine},
+      {item: 'minecraft:redstone', chance: 0.15},
     ],
-    experience: 0.8,
+    experience: 0.0,
   });
-};
-
-onEvent('recipes', event => {
-  crushingWheel(event);
-  enrichmentChamber(event);
-  ieCrusher(event);
-  mill(event);
-  pulverizer(event);
-  purifier(event);
 });
