e01f66570ba159a4a3f5d519a7b3c6b14fb18c8f
oitsjustjose <stovallj1995@gmail.com>
2022-03-10 08:15:30 -0600

    Clean up, remove betternether

---
+++ kubejs/server_scripts/HarmoniousEngineering/better_nether_remap.js
@@ -0,0 +1,162 @@
+onEvent('block.missing_mappings', event => {
+  event.remap('betternether:bone_block', 'minecraft:bone_block');
+  event.remap('betternether:netherrack_stalactite', 'quark:netherracak_speleothem');
+  event.remap('betternether:glowstone_stalactite', 'quark:netherracak_speleothem');
+  event.remap('betternether:blackstone_stalactite', 'quark:netherracak_speleothem');
+  event.remap('betternether:basalt_stalactite', 'quark:netherracak_speleothem');
+  event.remap('betternether:bone_stalactite', 'quark:netherracak_speleothem');
+  event.remap('betternether:blue_obsidian', 'minecraft:obsidian');
+  event.remap('betternether:soul_sandstone', 'minecraft:soul_sand');
+  event.remap('betternether:netherrack_moss', 'minecraft:netherrack');
+  event.remap('betternether:netherrack_mycelium', 'minecraft:netherrack');
+  event.remap('betternether:jungle_grass', 'minecraft:netherrack');
+  event.remap('betternether:mushroom_grass', 'minecraft:netherrack');
+  event.remap('betternether:sepia_mushroom_grass', 'minecraft:netherrack');
+  event.remap('betternether:swampland_glass', 'minecraft:netherrack');
+  event.remap('betternether:ceiling_mushrooms', 'minecraft:netherrack');
+  // Quartz Glass (Solid)
+  event.remap('betternether:quartz_glass_black', 'connectedglass:borderless_glass_black');
+  event.remap('betternether:quartz_glass_blue', 'connectedglass:borderless_glass_blue');
+  event.remap('betternether:quartz_glass_brown', 'connectedglass:borderless_glass_brown');
+  event.remap('betternether:quartz_glass_cyan', 'connectedglass:borderless_glass_cyan');
+  event.remap('betternether:quartz_glass_framed_black', 'connectedglass:borderless_glass_black');
+  event.remap('betternether:quartz_glass_framed_blue', 'connectedglass:borderless_glass_blue');
+  event.remap('betternether:quartz_glass_framed_brown', 'connectedglass:borderless_glass_brown');
+  event.remap('betternether:quartz_glass_framed_cyan', 'connectedglass:borderless_glass_cyan');
+  event.remap('betternether:quartz_glass_framed_gray', 'connectedglass:borderless_glass_gray');
+  event.remap('betternether:quartz_glass_framed_green', 'connectedglass:borderless_glass_green');
+  event.remap(
+    'betternether:quartz_glass_framed_light_blue',
+    'connectedglass:borderless_glass_light_blue'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_light_gray',
+    'connectedglass:borderless_glass_light_gray'
+  );
+  event.remap('betternether:quartz_glass_framed_lime', 'connectedglass:borderless_glass_lime');
+  event.remap(
+    'betternether:quartz_glass_framed_magenta',
+    'connectedglass:borderless_glass_magenta'
+  );
+  event.remap('betternether:quartz_glass_framed_orange', 'connectedglass:borderless_glass_orange');
+  event.remap('betternether:quartz_glass_framed_pink', 'connectedglass:borderless_glass_pink');
+  event.remap('betternether:quartz_glass_framed_purple', 'connectedglass:borderless_glass_purple');
+  event.remap('betternether:quartz_glass_framed_red', 'connectedglass:borderless_glass_red');
+  event.remap('betternether:quartz_glass_framed_white', 'connectedglass:borderless_glass_white');
+  event.remap('betternether:quartz_glass_framed_yellow', 'connectedglass:borderless_glass_yellow');
+  event.remap('betternether:quartz_glass_framed', 'connectedglass:borderless_glass');
+  event.remap('betternether:quartz_glass_gray', 'connectedglass:borderless_glass_gray');
+  event.remap('betternether:quartz_glass_green', 'connectedglass:borderless_glass_green');
+  event.remap('betternether:quartz_glass_light_blue', 'connectedglass:borderless_glass_light_blue');
+  event.remap('betternether:quartz_glass_light_gray', 'connectedglass:borderless_glass_light_gray');
+  event.remap('betternether:quartz_glass_lime', 'connectedglass:borderless_glass_lime');
+  event.remap('betternether:quartz_glass_magenta', 'connectedglass:borderless_glass_magenta');
+  event.remap('betternether:quartz_glass_orange', 'connectedglass:borderless_glass_orange');
+  event.remap('betternether:quartz_glass_pink', 'connectedglass:borderless_glass_pink');
+  event.remap('betternether:quartz_glass_purple', 'connectedglass:borderless_glass_purple');
+  event.remap('betternether:quartz_glass_red', 'connectedglass:borderless_glass_red');
+  event.remap('betternether:quartz_glass_white', 'connectedglass:borderless_glass_white');
+  event.remap('betternether:quartz_glass_yellow', 'connectedglass:borderless_glass_yellow');
+  event.remap('betternether:quartz_glass', 'connectedglass:borderless_glass');
+  // Quartz Panes
+  event.remap(
+    'betternether:quartz_glass_framed_pane_black',
+    'connectedglass:borderless_glass_black_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_blue',
+    'connectedglass:borderless_glass_blue_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_brown',
+    'connectedglass:borderless_glass_brown_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_cyan',
+    'connectedglass:borderless_glass_cyan_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_gray',
+    'connectedglass:borderless_glass_gray_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_green',
+    'connectedglass:borderless_glass_green_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_light_blue',
+    'connectedglass:borderless_glass_light_blue_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_light_gray',
+    'connectedglass:borderless_glass_light_gray_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_lime',
+    'connectedglass:borderless_glass_lime_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_magenta',
+    'connectedglass:borderless_glass_magenta_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_orange',
+    'connectedglass:borderless_glass_orange_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_pink',
+    'connectedglass:borderless_glass_pink_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_purple',
+    'connectedglass:borderless_glass_purple_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_red',
+    'connectedglass:borderless_glass_red_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_white',
+    'connectedglass:borderless_glass_white_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_framed_pane_yellow',
+    'connectedglass:borderless_glass_yellow_pane'
+  );
+  event.remap('betternether:quartz_glass_framed_pane', 'connectedglass:borderless_glass_pane');
+  event.remap('betternether:quartz_glass_pane_black', 'connectedglass:borderless_glass_black_pane');
+  event.remap('betternether:quartz_glass_pane_blue', 'connectedglass:borderless_glass_blue_pane');
+  event.remap('betternether:quartz_glass_pane_brown', 'connectedglass:borderless_glass_brown_pane');
+  event.remap('betternether:quartz_glass_pane_cyan', 'connectedglass:borderless_glass_cyan_pane');
+  event.remap('betternether:quartz_glass_pane_gray', 'connectedglass:borderless_glass_gray_pane');
+  event.remap('betternether:quartz_glass_pane_green', 'connectedglass:borderless_glass_green_pane');
+  event.remap(
+    'betternether:quartz_glass_pane_light_blue',
+    'connectedglass:borderless_glass_light_blue_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_pane_light_gray',
+    'connectedglass:borderless_glass_light_gray_pane'
+  );
+  event.remap('betternether:quartz_glass_pane_lime', 'connectedglass:borderless_glass_lime_pane');
+  event.remap(
+    'betternether:quartz_glass_pane_magenta',
+    'connectedglass:borderless_glass_magenta_pane'
+  );
+  event.remap(
+    'betternether:quartz_glass_pane_orange',
+    'connectedglass:borderless_glass_orange_pane'
+  );
+  event.remap('betternether:quartz_glass_pane_pink', 'connectedglass:borderless_glass_pink_pane');
+  event.remap(
+    'betternether:quartz_glass_pane_purple',
+    'connectedglass:borderless_glass_purple_pane'
+  );
+  event.remap('betternether:quartz_glass_pane_red', 'connectedglass:borderless_glass_red_pane');
+  event.remap('betternether:quartz_glass_pane_white', 'connectedglass:borderless_glass_white_pane');
+  event.remap(
+    'betternether:quartz_glass_pane_yellow',
+    'connectedglass:borderless_glass_yellow_pane'
+  );
+  event.remap('betternether:quartz_glass_pane', 'connectedglass:borderless_glass_pane');
+});
