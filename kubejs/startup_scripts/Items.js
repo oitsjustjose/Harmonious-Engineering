018a04e393160330250d054dc05b1dab27bb22ab
oitsjustjose <stovallj1995@gmail.com>
2022-06-22 13:33:21 -0500

    Add incomplete items, fix startup error

--- kubejs/startup_scripts/Items.js
+++ kubejs/startup_scripts/Items.js
@@ -3,8 +3,15 @@
 onEvent('item.registry', event => {
   event.create('crude_hammer').texture('kubejs:item/incomplete_hammer').maxStackSize(1);
   event
-    .create('incomplete_hammer')
+    .create('incomplete_hammer', 'create:sequenced_assembly')
     .texture('kubejs:item/incomplete_hammer')
-    .type('create:sequenced_assembly')
     .maxStackSize(1);
+
+  event
+    .create('incomplete_redstone_servo', 'create:sequenced_assembly')
+    .texture('kubejs:item/incomplete_redstone_servo');
+
+  event
+    .create('incomplete_rf_coil', 'create:sequenced_assembly')
+    .texture('kubejs:item/incomplete_rf_coil');
 });
