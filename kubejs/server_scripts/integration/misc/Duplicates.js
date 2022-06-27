be32c2f9d71486763570924c3d8e9f10aa831c60
oitsjustjose <stovallj1995@gmail.com>
2022-06-27 09:26:07 -0500

    Fix copper duplicates

--- kubejs/server_scripts/integration/misc/Duplicates.js
+++ kubejs/server_scripts/integration/misc/Duplicates.js
@@ -6,4 +6,7 @@ onEvent('recipes', event => {
   event.replaceInput({}, global.ie('slag'), '#forge:slag');
   event.replaceInput({}, 'industrialforegoing:plastic', '#forge:plastic');
   event.replaceInput({}, 'pneumaticcraft:plastic', '#forge:plastic');
+
+  event.remove({input: '#forge:nuggets/copper', output: '#forge:ingots/copper'});
+  event.shaped('minecraft:copper_ingot', ['XXX', 'XXX', 'XXX'], {X: '#forge:nuggets/copper'});
 });
