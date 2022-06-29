056231e21d5ff1e6269d46654be01d1ecb6ea2a3
oitsjustjose <stovallj1995@gmail.com>
2022-06-28 21:57:59 -0500

    Fix accidentally removed sequenced assembly recipe

--- kubejs/server_scripts/progression/3_CreateBrassAge.js
+++ kubejs/server_scripts/progression/3_CreateBrassAge.js
@@ -16,4 +16,43 @@ onEvent('recipes', event => {
     Item.of('#forge:ingots/brass'),
     Item.of('4x architects_palette:nether_brass_ingot')
   );
+
+  // Re-add precision mech recipe because it was removed by its inclusion in global.duplicateResources
+  event.custom({
+    type: 'create:sequenced_assembly',
+    ingredient: {tag: 'forge:plates/gold'},
+    transitionalItem: {item: 'create:incomplete_precision_mechanism'},
+    sequence: [
+      {
+        type: 'create:deploying',
+        ingredients: [{item: 'create:incomplete_precision_mechanism'}, {item: 'create:cogwheel'}],
+        results: [{item: 'create:incomplete_precision_mechanism'}],
+      },
+      {
+        type: 'create:deploying',
+        ingredients: [
+          {item: 'create:incomplete_precision_mechanism'},
+          {item: 'create:large_cogwheel'},
+        ],
+        results: [{item: 'create:incomplete_precision_mechanism'}],
+      },
+      {
+        type: 'create:deploying',
+        ingredients: [{item: 'create:incomplete_precision_mechanism'}, {tag: 'forge:nuggets/iron'}],
+        results: [{item: 'create:incomplete_precision_mechanism'}],
+      },
+    ],
+    results: [
+      {item: 'create:precision_mechanism', chance: 120.0},
+      {item: 'thermal:gold_plate', chance: 8.0},
+      {item: 'create:andesite_alloy', chance: 8.0},
+      {item: 'create:cogwheel', chance: 5.0},
+      {item: 'create:shaft', chance: 2.0},
+      {item: 'thermal:gold_dust', chance: 2.0},
+      {item: 'minecraft:gold_nugget', chance: 2.0},
+      {item: 'minecraft:iron_ingot'},
+      {item: 'minecraft:clock'},
+    ],
+    loops: 5,
+  });
 });
