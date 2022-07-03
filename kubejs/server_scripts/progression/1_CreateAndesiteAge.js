2ed7ac6cd9df7db3811a2019c2f8e56e697c9172
oitsjustjose <stovallj1995@gmail.com>
2022-07-03 12:05:20 -0500

    Fix andesite alloy being uncraftable

--- kubejs/server_scripts/progression/1_CreateAndesiteAge.js
+++ kubejs/server_scripts/progression/1_CreateAndesiteAge.js
@@ -1,15 +1,22 @@
 // Gate Early-game Create Behind PnC
 onEvent('recipes', event => {
   const CustomRecipeHandler = global.recipes(event);
-  event.remove({output: 'create:andesite_alloy'});
 
+  event.remove({id: 'pneumaticcraft:pressure_chamber/compressed_stone'});
+  CustomRecipeHandler.pressurizing(
+    [{type: 'pneumaticcraft:stacked_item', item: 'minecraft:stone', count: 1}],
+    [{item: 'pneumaticcraft:compressed_stone'}],
+    1.0
+  );
+
+  event.remove({output: 'create:andesite_alloy'});
   CustomRecipeHandler.pressurizing(
     [
       {type: 'pneumaticcraft:stacked_item', item: 'pneumaticcraft:ingot_iron_compressed', count: 1},
       {type: 'pneumaticcraft:stacked_item', item: 'minecraft:andesite', count: 2},
     ],
     [{item: 'create:andesite_alloy', count: 3}],
-    6.0
+    2.0
   );
 
   event.remove({output: 'create:mechanical_drill'});
