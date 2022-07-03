0fba9e1f705d0a997f7353655792ee6e28b70758
oitsjustjose <stovallj1995@gmail.com>
2022-07-03 12:05:12 -0500

    QOL XP bottle recipe

--- kubejs/server_scripts/integration/misc/Crafting.js
+++ kubejs/server_scripts/integration/misc/Crafting.js
@@ -2,6 +2,11 @@ onEvent('recipes', event => {
   // Remove sawdust flooring -- not progression but :shrug:
   event.remove({output: global.ie('sawdust')});
 
+  event.shapeless('1x minecraft:experience_bottle', [
+    '4x create:experience_nugget',
+    'minecraft:bottle',
+  ]);
+
   event.replaceInput({}, 'minecraft:rabbit_hide', '#forge:hides');
   event.remove({id: 'minecraft:leather'});
   event.shaped('minecraft:leather', ['XX', 'XX'], {X: 'minecraft:rabbit_hide'});
