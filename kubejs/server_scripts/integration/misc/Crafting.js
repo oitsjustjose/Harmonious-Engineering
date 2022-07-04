9492acc0b22734887838ea773b122a77e668a751
oitsjustjose <stovallj1995@gmail.com>
2022-07-03 23:31:34 -0500

    Decorative Blocks and Create Intg

--- kubejs/server_scripts/integration/misc/Crafting.js
+++ kubejs/server_scripts/integration/misc/Crafting.js
@@ -4,10 +4,18 @@ onEvent('recipes', event => {
 
   event.shapeless('1x minecraft:experience_bottle', [
     '4x create:experience_nugget',
-    'minecraft:bottle',
+    'minecraft:glass_bottle',
   ]);
 
   event.replaceInput({}, 'minecraft:rabbit_hide', '#forge:hides');
   event.remove({id: 'minecraft:leather'});
   event.shaped('minecraft:leather', ['XX', 'XX'], {X: 'minecraft:rabbit_hide'});
+
+  event.remove({output: 'decorative_blocks:rocky_dirt'});
+  event.shapeless('2x decorative_blocks:rocky_dirt', ['verdure:pebbles', 'minecraft:dirt']);
+  event.shapeless('4x decorative_blocks:rocky_dirt', [
+    'verdure:rock',
+    'minecraft:dirt',
+    'minecraft:dirt',
+  ]);
 });
