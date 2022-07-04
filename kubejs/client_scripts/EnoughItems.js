92b1f4be42b19b559bb07e1ddf7417b9594f927e
oitsjustjose <stovallj1995@gmail.com>
2022-07-03 23:30:45 -0500

    Fix failures in [J|R]EI HideItems

--- kubejs/client_scripts/EnoughItems.js
+++ kubejs/client_scripts/EnoughItems.js
@@ -1,5 +1,5 @@
 // priority: 0
-global.hideItems = [
+const items = [
   '/expandedstorage:*_present/',
   '/libraryferret:*/',
   'create:copper_sheet',
@@ -60,6 +60,67 @@ global.hideItems = [
   'expandedstorage:white_mini_present_with_sparrow',
   'expandedstorage:white_mini_present',
   'expandedstorage:wood_mini_chest_with_sparrow',
+  'immersiveengineering:coal_coke',
+  'immersiveengineering:coke',
+  'immersiveengineering:deepslate_ore_lead',
+  'immersiveengineering:deepslate_ore_nickel',
+  'immersiveengineering:deepslate_ore_silver',
+  'immersiveengineering:deepslate_ore_uranium',
+  'immersiveengineering:dust_constantan',
+  'immersiveengineering:dust_copper',
+  'immersiveengineering:dust_electrum',
+  'immersiveengineering:dust_gold',
+  'immersiveengineering:dust_iron',
+  'immersiveengineering:dust_lead',
+  'immersiveengineering:dust_nickel',
+  'immersiveengineering:dust_silver',
+  'immersiveengineering:dust_uranium',
+  'immersiveengineering:dust_wood',
+  'immersiveengineering:furnace_heater',
+  'immersiveengineering:ingot_constantan',
+  'immersiveengineering:ingot_electrum',
+  'immersiveengineering:ingot_lead',
+  'immersiveengineering:ingot_nickel',
+  'immersiveengineering:ingot_silver',
+  'immersiveengineering:ingot_uranium',
+  'immersiveengineering:nugget_constantan',
+  'immersiveengineering:nugget_electrum',
+  'immersiveengineering:nugget_lead',
+  'immersiveengineering:nugget_nickel',
+  'immersiveengineering:nugget_silver',
+  'immersiveengineering:nugget_uranium',
+  'immersiveengineering:ore_lead',
+  'immersiveengineering:ore_nickel',
+  'immersiveengineering:ore_silver',
+  'immersiveengineering:ore_uranium',
+  'immersiveengineering:plate_constantan',
+  'immersiveengineering:plate_copper',
+  'immersiveengineering:plate_electrum',
+  'immersiveengineering:plate_gold',
+  'immersiveengineering:plate_iron',
+  'immersiveengineering:plate_lead',
+  'immersiveengineering:plate_nickel',
+  'immersiveengineering:plate_silver',
+  'immersiveengineering:plate_uranium',
+  'immersiveengineering:raw_block_uranium',
+  'immersiveengineering:raw_lead',
+  'immersiveengineering:raw_nickel',
+  'immersiveengineering:raw_silver',
+  'immersiveengineering:raw_uranium',
+  'immersiveengineering:sheetmetal_uranium',
+  'immersiveengineering:slab_sheetmetal_uranium',
+  'immersiveengineering:slab_storage_uranium',
+  'immersiveengineering:slag',
+  'immersiveengineering:storage_constantan',
+  'immersiveengineering:storage_electrum',
+  'immersiveengineering:storage_lead',
+  'immersiveengineering:storage_nickel',
+  'immersiveengineering:storage_silver',
+  'immersiveengineering:storage_steel',
+  'immersiveengineering:storage_uranium',
+  'immersiveengineering:watermill',
+  'immersiveengineering:waterwheel_segment',
+  'immersiveengineering:windmill',
   'industrialforegoing:diamond_gear',
   'industrialforegoing:dryrubber',
   'industrialforegoing:gold_gear',
@@ -68,6 +129,7 @@ global.hideItems = [
   'industrialforegoing:plastic',
   'industrialforegoing:tinydryrubber',
   'minecraft:chest',
+  'moreminecarts:silica_steel_mix',
   'pipez:gas_pipe',
   'rftoolsbase:crafting_card',
   'rftoolsbase:filter_module',
@@ -81,73 +143,31 @@ global.hideItems = [
   'thermal:saw_blade',
   'torchmaster:feral_flare_lantern',
   'torchmaster:frozen_pearl',
-  global.ie('coal_coke'),
-  global.ie('coke'),
-  global.ie('deepslate_ore_lead'),
-  global.ie('deepslate_ore_nickel'),
-  global.ie('deepslate_ore_silver'),
-  global.ie('deepslate_ore_uranium'),
-  global.ie('dust_constantan'),
-  global.ie('dust_copper'),
-  global.ie('dust_electrum'),
-  global.ie('dust_gold'),
-  global.ie('dust_iron'),
-  global.ie('dust_lead'),
-  global.ie('dust_nickel'),
-  global.ie('dust_silver'),
-  global.ie('dust_uranium'),
-  global.ie('dust_wood'),
-  global.ie('furnace_heater'),
-  global.ie('ingot_constantan'),
-  global.ie('ingot_electrum'),
-  global.ie('ingot_lead'),
-  global.ie('ingot_nickel'),
-  global.ie('ingot_silver'),
-  global.ie('ingot_uranium'),
-  global.ie('nugget_constantan'),
-  global.ie('nugget_electrum'),
-  global.ie('nugget_lead'),
-  global.ie('nugget_nickel'),
-  global.ie('nugget_silver'),
-  global.ie('nugget_uranium'),
-  global.ie('ore_lead'),
-  global.ie('ore_nickel'),
-  global.ie('ore_silver'),
-  global.ie('ore_uranium'),
-  global.ie('plate_constantan'),
-  global.ie('plate_copper'),
-  global.ie('plate_electrum'),
-  global.ie('plate_gold'),
-  global.ie('plate_iron'),
-  global.ie('plate_lead'),
-  global.ie('plate_nickel'),
-  global.ie('plate_silver'),
-  global.ie('plate_uranium'),
-  global.ie('raw_block_uranium'),
-  global.ie('raw_lead'),
-  global.ie('raw_nickel'),
-  global.ie('raw_silver'),
-  global.ie('raw_uranium'),
-  global.ie('sheetmetal_uranium'),
-  global.ie('slab_sheetmetal_uranium'),
-  global.ie('slab_storage_uranium'),
-  global.ie('slag'),
-  global.ie('storage_constantan'),
-  global.ie('storage_electrum'),
-  global.ie('storage_lead'),
-  global.ie('storage_nickel'),
-  global.ie('storage_silver'),
-  global.ie('storage_steel'),
-  global.ie('storage_uranium'),
-  global.ie('watermill'),
-  global.ie('waterwheel_segment'),
-  global.ie('windmill'),
 ];
 
-onEvent('rei.hide.items', event => {
-  global.hideItems.forEach(x => event.hide(x));
+onEvent('jei.hide.items', event => {
+  items.forEach(x => {
+    try {
+      console.log(x);
+      event.hide(x);
+    } catch (e) {
+      console.error(e);
+    }
+  });
 });
 
-onEvent('jei.hide.items', event => {
-  global.hideItems.forEach(x => event.hide(x));
+onEvent('rei.hide.items', event => {
+  items.forEach(x => {
+    try {
+      console.log(x);
+      event.hide(x);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+});
+
+onEvent('rei.remove.categories', event => {
+  console.log(event.getCategoryIds());
+  event.remove('minecraft:plugins/tag');
 });
